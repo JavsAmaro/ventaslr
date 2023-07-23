41f729b72497d3df60902f9c6216bf98cc6d2ccc
Javier Amaro <javier.amaro101@gmail.com>
2023-07-23 16:16:33 -0600

    eliminacion de errores de archivos basura htaccess
    eliminacion de errores de archivos htaccess

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,11 @@
 <?php
- $db = mysqli_connect('localhost', 'punto_de_ventas', '12345') or
+//  $db = mysqli_connect('localhost', 'punto_de_ventas', '12345') or
+//         die ('Unable to connect. Check your connection parameters.');
+//         mysqli_select_db($db, 'prince' ) or die(mysqli_error($db));
+
+
+        $db = mysqli_connect('localhost', 'root', '') or
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'prince' ) or die(mysqli_error($db));
+
 ?>
